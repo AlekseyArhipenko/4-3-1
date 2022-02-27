87a71c404b74adfaa62bc403f027725ec398c65b
AlekseyArhipenko <arhipenko1990@inbox.ru>
2022-02-27 13:04:19 +0300

    finish project

--- src/test/java/ru/netology/NewCardOrderTest.java
+++ src/test/java/ru/netology/NewCardOrderTest.java
@@ -23,7 +23,7 @@ public class NewCardOrderTest {
 
     @BeforeAll
     public static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
 
     }
 
